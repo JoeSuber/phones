601dc5f9b9ea20a9a14c57cbc1ee0a8acbb7b3c1
Joseph Suber <joesuber@gmail.com>
2017-06-08 13:28:02 -0500

    hellooo

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    with open(os.path.join(os.getcwd(), "test.txt"), 'wb') as fob:
+        fob.write("Hey I think this is a file")
     return "<h1> HELOOoooo </h1>"
 
 if __name__ == "__main__":
