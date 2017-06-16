c4b36a53944c55ea2490c45b919490f9a0655f08
Joseph Suber <joesuber@gmail.com>
2017-06-16 14:04:35 -0500

    sub domians

--- app.py
+++ app.py
@@ -642,5 +642,5 @@ def dumpty():
 
 
 if __name__ == '__main__':
-    # app.run(debug=False)
-    app.run(host='0.0.0.0')
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
