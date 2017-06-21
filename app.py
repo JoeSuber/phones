0daf87fd840ead1c0d9a6a0c3c22c4bf206f78ea
Joseph Suber <joesuber@gmail.com>
2017-06-21 14:17:24 -0500

    delete port=5000

--- app.py
+++ app.py
@@ -635,4 +635,4 @@ def send_report(email, attachment_fn, sender=None, subject='Overdue Devices Repo
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', debug=False)
