da00da4f5f7ca06bcc0b48b33d2c0423c444c42c
Joseph Suber <joesuber@gmail.com>
2017-06-22 16:58:46 -0500

    added sample mail script

--- trymail.py
+++ trymail.py
@@ -4,16 +4,16 @@ sender = 'joe.suber@dvtandc.com'
 receivers = ['support@contegix.com', 'joe.suber@dvtandc.com']
 
 for receiver in receivers:
-    message = """From: <{}>
-    To: <{}>
+    message = """From: Joe <{}>
+    To: Testees <{}>
     Subject: SMTP e-mail test
     
-    This is a test e-mail message that hopefully works now and won't end up in spam
+    This is a test e-mail message that hopefully works now and won't end up in spam.
     """.format(sender, receiver)
 
     try:
        smtpObj = smtplib.SMTP('localhost')
-       smtpObj.sendmail(sender, receiver, message)
+       smtpObj.sendmail(sender, [receiver], message)
        print("Successfully sent email")
     except smtplib.SMTPException:
        print("Error: unable to send email")
