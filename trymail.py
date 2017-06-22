11a6e8c7dcad042d845c9256fe7b6a08bb7e22ba
Joseph Suber <joesuber@gmail.com>
2017-06-22 17:07:10 -0500

    added sample mail script

--- trymail.py
+++ trymail.py
@@ -4,11 +4,9 @@ sender = 'joe.suber@dvtandc.com'
 receivers = ['support@contegix.com', 'joe.suber@dvtandc.com']
 
 for receiver in receivers:
-    message = """From: Joe <{}>
-    
-    To: Testees <{}>
-    
-    Subject: SMTP e-mail test
+    message = """From: Joe <{}> \n\r
+    To: Testees <{}> \n\r
+    Subject: SMTP e-mail test \n\r
     
     This is a test e-mail message that hopefully works now and won't end up in spam.
     """.format(sender, receiver)
