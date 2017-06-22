ffe4b5d42fe9229db69c11040eb3fb2c79f51042
Joseph Suber <joesuber@gmail.com>
2017-06-22 16:54:21 -0500

    added sample mail script

--- trymail.py
+++ trymail.py
@@ -4,8 +4,8 @@ sender = 'joe.suber@dvtandc.com'
 receivers = ['support@contegix.com', 'joe.suber@dvtandc.com']
 
 for receiver in receivers:
-    message = """From: {}
-    To: {}
+    message = """From: <{}>
+    To: <{}>
     Subject: SMTP e-mail test
     
     This is a test e-mail message that hopefully works now and won't end up in spam
